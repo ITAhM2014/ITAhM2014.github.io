3848d04f64bddb46cbb8bf5731ff061ccf64b6cd
itahm2014 <tomato322@daum.net>
2019-04-08 10:36:28 +0900

    3.0 업그레이드

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,16 +2,37 @@
 
 const TRAFFIC_INTERVAL = 3000;
 
+function parse (link, message) {
+	const result = {};
+
+	if ("ifOperStatus" in link) {
+		result.status = link.ifOperStatus === 1? 1: -1;
+	}
+
+	if (result.status === 1) {
+		if ("option" in line) {
+			result.color = line.option.color;
+		}
+		
+		if ("ifSpeed" in link && link.ifSpeed > 0) {
+			if ("ifInBPS" in link) {
+				result.in = Math.round(link.ifInBPS *10 / ("speed" in link && link.speed > 0? link.speed: link.ifSpeed)) *10;
+			}
+
+			if ("ifOutBPS" in link) {
+				result.out = Math.round(link.ifOutBPS *10 / ("speed" in link && link.speed > 0? link.speed: link.ifSpeed)) *10;
+			}
+		}
+	}
+
+	return result;
+}
+
 self.onmessage = e => {
 	const traffic = e.data;
-	var line, ar, id1, id2, data, message;
+	var line, ar, id1, id2, message;
 
 	for (let id in traffic) {
-		message = {
-			id: id,
-			status: 0,
-		};
-
 		ar = id.split(".");
 
 		if (ar.length != 3 || ar[0] != "line") {
@@ -23,35 +44,32 @@ self.onmessage = e => {
 
 		line = traffic[id];
 	
-		if (id1 in line) {
-			data = line[id1];
-			
-			if ("ifOperStatus" in data) {
-				message.status = line[id1].ifOperStatus === 1? 1: -1;
-			}
-		}
-		else if (id2 in line) {
-			data = line[id2];
-			
-			if ("ifOperStatus" in data) {
-				message.status = line[id2].ifOperStatus === 1? 1: -1;
-			}
-		}
-		
-		if (message.status === 1) {
-			if ("option" in line) {
-				message.color = line.option.color;
-			}
-			
-			if ("ifSpeed" in data && data.ifSpeed > 0) {
-				if ("ifInBPS" in data) {
-					message.in = Math.round(data.ifInBPS *10 / ("speed" in data && data.speed > 0? data.speed: data.ifSpeed)) *10;
-				}
+		if (Array.isArray(line[id1])) {
+			message = {
+				id: id,
+				link: []
+			};
 
-				if ("ifOutBPS" in data) {
-					message.out = Math.round(data.ifOutBPS *10 / ("speed" in data && data.speed > 0? data.speed: data.ifSpeed)) *10;
+			line[id1].forEach((link, i) => {
+				if (link) {
+					message.link[i] = parse(link, message);
 				}
-			}
+				else if (line[id2][i]) {
+					message.link[i] = parse(line[id2][i]);
+				}
+			});
+		}
+		else if (line[id1]) {
+			message = {
+				id: id,
+				link: parse(line[id1])
+			};
+		}
+		else if (line[id2]) {
+			message = {
+				id: id,
+				link: parse(line[id2])
+			};
 		}
 
 		postMessage(message);
