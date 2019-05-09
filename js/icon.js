4047af4910d9e2f51846a1362d305bc773afac3f
itahm2014 <tomato322@daum.net>
2019-05-09 15:38:05 +0900

    3D 적용

--- js/icon.js
+++ js/icon.js
@@ -4,148 +4,247 @@ ITAhM.iconData = {
     "server": {
         "group": "server",
         "src": "img/svg/server.svg",
-        "disabled": "img/svg/disabled/server.svg"
+        "disabled": "img/svg/disabled/server.svg",
+        unit: 1,
+        color: "",
+        texture: "/img/3d/server-1u.png",
+        top: "/img/3d/server-top.png"
     },
     "mainframe": {
         "group": "server",
         "alt": "mainframe",
         "src": "img/svg/mainframe.svg",
-        "disabled": "img/svg/disabled/mainframe.svg"
+        "disabled": "img/svg/disabled/mainframe.svg",
+        unit: 1,
+        color: "",
+        texture: "/img/3d/server-1u.png",
+        top: "/img/3d/server-top.png"
     },
     "database": {
         "group": "server",
         "alt": "database",
         "src": "img/svg/database.svg",
-        "disabled": "img/svg/disabled/database.svg"
+        "disabled": "img/svg/disabled/database.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "storage": {
         "group": "server",
         "src": "img/svg/storage.svg",
-        "disabled": "img/svg/disabled/storage.svg"
+        "disabled": "img/svg/disabled/storage.svg",
+        unit: 2,
+        color: "",
+        texture: "/img/3d/storage.png",
+        top: "/img/3d/storage-top.png"
     },
     "network": {
         "group": "network",
         "src": "img/svg/network.svg",
-        "disabled": "img/svg/disabled/network.svg"
+        "disabled": "img/svg/disabled/network.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "nms": {
         "group": "network",
         "src": "img/svg/nms.svg",
-        "disabled": "img/svg/disabled/nms.svg"
+        "disabled": "img/svg/disabled/nms.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "workgroup switch": {
         "group": "network",
         "src": "img/svg/workgroup.svg",
-        "disabled": "img/svg/disabled/workgroup.svg"
+        "disabled": "img/svg/disabled/workgroup.svg",
+        "texture": "/img/3d/wg.png",
+        top: "/img/3d/wg-top.png",
+        color: "",
+        unit: 2
     },
     "layer4 switch": {
         "group": "network",
         "src": "img/svg/l4switch.svg",
-        "disabled": "img/svg/disabled/l4switch.svg"
+        "disabled": "img/svg/disabled/l4switch.svg",
+        unit: 6,
+        color: "",
+        texture: "/img/3d/l4.png",
+        top: "/img/3d/ace-top.png"
     },
     "layer3 switch": {
         "group": "network",
         "src": "img/svg/l3switch.svg",
-        "disabled": "img/svg/disabled/l3switch.svg"
+        "disabled": "img/svg/disabled/l3switch.svg",
+        unit: 6,
+        color: "",
+        texture: "/img/3d/l3.png",
+        top: "/img/3d/routing-top.png"
     },
     "router": {
         "group": "network",
         "src": "img/svg/router.svg",
-        "disabled": "img/svg/disabled/router.svg"
+        "disabled": "img/svg/disabled/router.svg",
+        unit: 2,
+        color: "",
+        texture: "/img/3d/router.png",
+        top: "/img/3d/router-top.png"
     },
     "firewall": {
         "group": "network",
         "src": "img/svg/firewall.svg",
-        "disabled": "img/svg/disabled/firewall.svg"
+        "disabled": "img/svg/disabled/firewall.svg",
+        texture: "/img/3d/firewall.png",
+        top: "/img/3d/firewall-top.png",
+        color: "",
+        unit: 4
     },
     "vpn gateway": {
         "group": "network",
         "src": "img/svg/vpn.svg",
-        "disabled": "img/svg/disabled/vpn.svg"
+        "disabled": "img/svg/disabled/vpn.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "vpn concentrator": {
         "group": "network",
         "src": "img/svg/concentrator.svg",
-        "disabled": "img/svg/disabled/concentrator.svg"
+        "disabled": "img/svg/disabled/concentrator.svg",
+        unit: 2,
+        color: "",
+        texture: "/img/3d/concentrator.png",
+        top: "/img/3d/concentrator-top.png"
     },
     "wireless": {
         "group": "network",
         "src": "img/svg/wireless.svg",
-        "disabled": "img/svg/disabled/wireless.svg"
+        "disabled": "img/svg/disabled/wireless.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "accesspoint": {
         "group": "network",
         "src": "img/svg/accesspoint.svg",
-        "disabled": "img/svg/disabled/accesspoint.svg"
+        "disabled": "img/svg/disabled/accesspoint.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "accelerator": {
         "group": "network",
         "src": "img/svg/accelerator.svg",
-        "disabled": "img/svg/disabled/accelerator.svg"
+        "disabled": "img/svg/disabled/accelerator.svg",
+        unit: 4,
+        color: "",
+        texture: "/img/3d/accelerator.png",
+        top: "/img/3d/accelerator-top.png"
     },
     "nac": {
         "group": "network",
         "src": "img/svg/nac.svg",
-        "disabled": "img/svg/disabled/nac.svg"
+        "disabled": "img/svg/disabled/nac.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "printer": {
     	"group": "etc",
     	"src": "img/svg/printer.svg",
-    	"disabled": "img/svg/disabled/printer.svg"
+        "disabled": "img/svg/disabled/printer.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "office": {
         "group": "group",
         "src": "img/svg/office.svg",
-        "disabled": "img/svg/disabled/office.svg"
+        "disabled": "img/svg/disabled/office.svg",
+        texture: "/img/3d/office.png",
+        color: 0x333333,
+        unit: 8
     },
     "cloud": {
         "group": "etc",
         "src": "img/svg/cloud.svg",
-        "disabled": "img/svg/disabled/cloud.svg"
+        "disabled": "img/svg/disabled/cloud.svg",
+        unit: 1,
+        color: "",
+        texture: "/img/3d/cloud.png",
+        top: "/img/3d/cloud-top.png"
     },
     "unknown": {
         "group": "etc",
         "src": "img/svg/unknown.svg",
-        "disabled": "img/svg/disabled/unknown.svg"
+        "disabled": "img/svg/disabled/unknown.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "phone": {
         "group": "etc",
         "src": "img/svg/phone.svg",
-        "disabled": "img/svg/disabled/phone.svg"
+        "disabled": "img/svg/disabled/phone.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "laptop": {
         "group": "etc",
         "src": "img/svg/laptop.svg",
-        "disabled": "img/svg/disabled/laptop.svg"
+        "disabled": "img/svg/disabled/laptop.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "cctv": {
         "group": "etc",
         "src": "img/svg/camera.svg",
-        "disabled": "img/svg/disabled/camera.svg"
+        "disabled": "img/svg/disabled/camera.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "ups": {
         "group": "etc",
         "src": "img/svg/ups.svg",
-        "disabled": "img/svg/disabled/ups.svg"
+        "disabled": "img/svg/disabled/ups.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "video recorder": {
     	"group": "etc",
         "src": "img/svg/videorecorder.svg",
-        "disabled": "img/svg/disabled/videorecorder.svg"
+        "disabled": "img/svg/disabled/videorecorder.svg",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "security": {
     	"group": "etc",
         "src": "img/svg/security.svg",
-        "disabled": "img/svg/disabled/security.svg"
+        "disabled": "img/svg/disabled/security.svg",
+        unit: 1,
+        color: "",
+        texture: "/img/3d/security.png",
+        top: "/img/3d/security-top.png"
     },
     "group": {
         "group": "group",
         "src": "img/group.png",
-        "disabled": "img/group.png"
+        "disabled": "img/group.png",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     },
     "application": {
         "group": "application",
         "src": "img/setting.png",
-        "disabled": "img/test.png"
+        "disabled": "img/test.png",
+        unit: 1,
+        color: "0xdddddd",
+        texture: ""
     }
 };
